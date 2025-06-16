549031a577fb48cd92155849e497cb8666714535
mrpete13 <mattrpete@pm.me>
2025-06-15 22:00:05 -0400

    progress on rewrite

--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,85 @@
+use byte_unit::Byte;
 use std::error::Error;
-use std::fs;
-use std::io;
-use std::process;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
+use std::sync::Arc;
 use std::thread;
-
-pub struct Config {
-    path: String,
-    total_size: u32, // needs to be converted from String
-    dir_count: u32,
-    file_size: u32,
-    thread_count: u16,
-    random_data: bool,
-}
+use std::time::Instant;
 
 fn cpu_count() -> u16 {
     num_cpus::get() as u16
 }
 
-fn to_bytes(size: u32) -> u32 {
-    todo!("Convert human-readable size to bytes");
-    // num * 1024 * 1024
-}
-
-fn calc_file_count(total_size: u32, file_size: u32) -> u32 {
-    todo!("Implement file count calculation");
-    //total_size.to_bytes() / file_size
-}
-
-fn make_files(config: &Config) -> Result<(), io::Error> {
-    todo!("Implement make_files function");
-    // Implementation of make_files function
-    Ok(())
-}
-
-impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.is_empty() {
-            return Err("Not enough arguments");
-        }
-
-        let path = args[1].clone();
-        let total_size = args[2].parse().unwrap_or(1); //defaults to 1 GiB
-        let dir_count = args[3].parse().unwrap_or(5); // defaults to 5 dirs
-        let file_size = args[4].parse().unwrap_or(256); // defaults to 256 KiB
-        let thread_count = args[4].parse().unwrap_or(cpu_count()); //defaults to max thread count
-        let random_data = args[5].parse().unwrap_or(false);
-
-        Ok(Config {
-            path,
-            total_size,
-            dir_count,
-            file_size,
-            thread_count,
-            random_data,
-        })
+fn parse_size(size_string: &str) -> Result<u64, Box<dyn Error>> {
+    let bytes = Byte::parse_str(size_string, true).unwrap().as_u64();
+    let bytes_u128: u128 = bytes as u128;
+    if bytes_u128 > u64::MAX as u128 {
+        Err("Size exceeds maximum value")?
     }
+    if bytes_u128 == 0 {
+        Err("Size cannot be zero")?
+    }
+    Ok(bytes)
+}
+
+fn calc_files_per_dir(total_size: u64, file_size_bytes: u64, dir_count: u16) -> u64 {
+    // todo!("Implement Result<> for error handling");
+    let total_files = total_size / file_size_bytes;
+    total_files / dir_count as u64
+}
+
+pub fn make_files(
+    path: Option<&PathBuf>,
+    total_size_bytes: Option<String>,
+    file_size_bytes: Option<String>,
+    dir_count: Option<u16>,
+    thread_count: Option<u16>,
+    _random_data: Option<bool>,
+) -> Result<(), Box<dyn Error>> {
+    let total_size = parse_size(total_size_bytes.as_ref().unwrap())?;
+    let file_size_str = file_size_bytes.as_ref().unwrap();
+    let file_size = parse_size(file_size_str)?;
+    println!("CPU Core count: {}", cpu_count());
+    let threads = thread_count.unwrap_or(4);
+    // todo!("Implement check to ensure system has enough RAM for buffer size of larger files");
+    let buffer_size = file_size; // Use file_size directly
+    let buffer = Arc::new(vec![0u8; buffer_size as usize]); // Create a Vec<u8> of proper size
+    let start_time = Instant::now();
+
+    let path = match path {
+        Some(p) => p.clone(),
+        None => PathBuf::from("/tmp/dirs"),
+    };
+
+    let dir_file_count = calc_files_per_dir(total_size, file_size, dir_count.unwrap_or(4));
+    let chunk_size = dir_file_count / threads as u64;
+
+    let mut handles: Vec<thread::JoinHandle<Result<(), std::io::Error>>> =
+        Vec::with_capacity(threads as usize);
+    for i in 0..threads {
+        let start = i as u64;
+        let end = start + chunk_size;
+        let path_clone = path.clone();
+        let buffer_clone = buffer.clone();
+        let handle = thread::spawn(move || {
+            for file_index in start..end {
+                let file_name = format!("file_{}", file_index);
+                let file_path = path_clone.join(file_name);
+                let mut file = File::create(file_path)?;
+                file.write_all(&buffer_clone)?;
+            }
+            Ok(())
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap()?;
+    }
+
+    let elapsed_time = start_time.elapsed();
+    println!("Elapsed time: {:?}", elapsed_time);
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,46 @@
-use manyfiles::Config;
-use std::env;
-use std::fs;
-use std::io;
-use std::process;
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+pub struct Args {
+    #[clap(short = 'p', long = "path", default_value = "/tmp/dirs")]
+    path: Option<PathBuf>,
+
+    #[clap(short = 's', long = "size", default_value = "1GiB")]
+    total_size_bytes: Option<String>,
+
+    #[clap(short = 'd', long = "numdirs", default_value = "4")]
+    dir_count: Option<u16>,
+
+    #[clap(short = 'f', long = "filesize", default_value = "256KiB")]
+    file_size_bytes: Option<String>,
+
+    #[clap(short = 'j', long = "threads", default_value = "4")]
+    thread_count: Option<u16>,
+
+    #[clap(short = 'r', long = "random", default_value = "false")]
+    random_data: Option<bool>,
+}
+
+// todo!(
+// "
+// - Cumulative size isn't being created.
+// - Files aren't being created in subdirectories.
+// "
+// );
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
+    let args = Args::parse();
+    if let Err(e) = manyfiles::make_files(
+        args.path.as_ref(),
+        args.total_size_bytes,
+        args.file_size_bytes,
+        args.dir_count,
+        args.thread_count,
+        args.random_data,
+    ) {
+        eprintln!("Error: {}", e);
+        std::process::exit(1);
+    }
 }
