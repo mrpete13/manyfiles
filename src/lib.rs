e252c1dc5bf2fd77d62b0c6800d522426382ff49
Matt Petersen <mattrpete@pm.me>
2025-06-18 23:27:04 -0400

    created function to make directories. still need to refactor the main function to account for this.
    also still need to implement multithreading.

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ use byte_unit::Byte;
 use std::error::Error;
 use std::fs::File;
 use std::io::Write;
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::sync::Arc;
 use std::thread;
 use std::time::Instant;
@@ -23,63 +23,58 @@ fn parse_size(size_string: &str) -> Result<u64, Box<dyn Error>> {
     Ok(bytes)
 }
 
-fn calc_files_per_dir(total_size: u64, file_size_bytes: u64, dir_count: u16) -> u64 {
+fn calc_files_per_dir(
+    total_size: u64,
+    file_size_bytes: u64,
+    dir_count: u16,
+) -> Result<u64, Box<dyn Error>> {
     // todo!("Implement Result<> for error handling");
     let total_files = total_size / file_size_bytes;
-    total_files / dir_count as u64
+    Ok(total_files / dir_count as u64)
 }
 
-pub fn make_files(
+pub fn make_file(
     path: Option<&PathBuf>,
-    total_size_bytes: Option<String>,
-    file_size_bytes: Option<String>,
-    dir_count: Option<u16>,
-    thread_count: Option<u16>,
+    file_index: u16,
+    buffer: &Arc<Vec<u8>>,
+    file_size: u64,
     _random_data: Option<bool>,
 ) -> Result<(), Box<dyn Error>> {
-    let total_size = parse_size(total_size_bytes.as_ref().unwrap())?;
-    let file_size_str = file_size_bytes.as_ref().unwrap();
-    let file_size = parse_size(file_size_str)?;
-    println!("CPU Core count: {}", cpu_count());
-    let threads = thread_count.unwrap_or(4);
-    // todo!("Implement check to ensure system has enough RAM for buffer size of larger files");
-    let buffer_size = file_size; // Use file_size directly
-    let buffer = Arc::new(vec![0u8; buffer_size as usize]); // Create a Vec<u8> of proper size
+    let mut file: File = File::create(path.unwrap().join(format!("file_{}", file_index)))?;
+
     let start_time = Instant::now();
+    let mut bytes_written = 0;
 
-    let path = match path {
-        Some(p) => p.clone(),
-        None => PathBuf::from("/tmp/dirs"),
-    };
-
-    let dir_file_count = calc_files_per_dir(total_size, file_size, dir_count.unwrap_or(4));
-    let chunk_size = dir_file_count / threads as u64;
-
-    let mut handles: Vec<thread::JoinHandle<Result<(), std::io::Error>>> =
-        Vec::with_capacity(threads as usize);
-    for i in 0..threads {
-        let start = i as u64;
-        let end = start + chunk_size;
-        let path_clone = path.clone();
-        let buffer_clone = buffer.clone();
-        let handle = thread::spawn(move || {
-            for file_index in start..end {
-                let file_name = format!("file_{}", file_index);
-                let file_path = path_clone.join(file_name);
-                let mut file = File::create(file_path)?;
-                file.write_all(&buffer_clone)?;
-            }
-            Ok(())
-        });
-        handles.push(handle);
-    }
-
-    for handle in handles {
-        handle.join().unwrap()?;
+    while bytes_written < file_size {
+        let bytes_to_write = std::cmp::min(file_size - bytes_written, buffer.len() as u64);
+        file.write_all(&buffer[..bytes_to_write as usize])?;
+        bytes_written += bytes_to_write;
     }
 
     let elapsed_time = start_time.elapsed();
     println!("Elapsed time: {:?}", elapsed_time);
 
+    Ok(())
+}
+
+pub fn make_dirs(
+    path: &Path,
+    dir_index: u16,
+    files_per_dir: u64,
+    file_size_bytes: Option<String>,
+) -> Result<(), Box<dyn Error>> {
+    let file_size = parse_size(file_size_bytes.expect("File size is required").as_str())?;
+    let buffer = Arc::new(vec![0; 1024 * 1024]);
+    let dir_path: PathBuf = path.join(format!("dir_{}", dir_index));
+    std::fs::create_dir_all(&dir_path)?;
+    for file_index in 0..files_per_dir {
+        make_file(
+            Some(&dir_path),
+            file_index.try_into().unwrap(),
+            &buffer,
+            file_size,
+            None,
+        )?;
+    }
     Ok(())
 }
